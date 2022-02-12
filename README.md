d2ccbdd416a068a192332a3ff8d8f8e792c4ed5f
Benjamin Brandt Ohrt (ADM) <ADM-BIO@prod.pfaintern.dk>
2022-02-12 21:26:25 +0100

    SimpleHTTP 1.0

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # WebsteR
+
+Is a RUST-project with purpose of implementing server-side applications for:
+> HTTP [RFC2616](https://datatracker.ietf.org/doc/html/rfc2616) \
+> HTTPS [RFC2818](https://datatracker.ietf.org/doc/html/rfc2818) \
+> WEBSOCKET [RFC6455](https://datatracker.ietf.org/doc/html/rfc6455)
+
+## Program
+
+### HTTP
+This HTTP server will be made doing the RUST tutorial : Final Project: Building af Multithreaded Web Server. Where plan is the following:
+
+1. Learn a bit about TCP and HTTP.
+2. Listen for TCP connections on a socket.
+3. Parse a small number of HTTP requests.
+4. Create a proper HTTP response.
+5. Improve the throughput of our server with a thread pool.
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs;
+
+fn main() 
+    {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming()
+        {
+        /* unwrap
+        For now, our handling of the stream consists of calling unwrap to terminate our program if the stream has 
+        any errors; if there aren’t any errors, the program prints a message. We’ll add more functionality for the 
+        success case in the next listing. The reason we might receive errors from the incoming method when a client 
+        connects to the server is that we’re not actually iterating over connections. Instead, we’re iterating over 
+        connection attempts. The connection might not be successful for a number of reasons, many of them operating 
+        system specific. For example, many operating systems have a limit to the number of simultaneous open 
+        connections they can support; new connection attempts beyond that number will produce an error until
+        some of the open connections are closed.
+        */
+        let _stream = stream.unwrap();
+
+        handle_connection(_stream);
+        }
+    println!("Connection established!");
+    }
+
+fn handle_connection(mut stream: TcpStream)
+    {
+    let mut buffer = [0; 1024];
+    
+    let get = b"GET /index HTTP/1.1\r\n";
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    if buffer.starts_with(get)
+        {
+        let contents = fs::read_to_string("public_html//index.html").unwrap();
+
+        /* format!
+        Next, we use format! to add the file’s contents as the body of the success response. 
+        To ensure a valid HTTP response, we add the 
+        Content-Length header which is set to the size of our response body, 
+        in this case the size of hello.html.
+        */
+        let response = 
+                format!(
+                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                contents.len(),
+                contents
+                );
+
+        /*
+        Because the write operation could fail, we use unwrap on any error result as before. 
+        Again, in a real application you would add error handling here. 
+        Finally, flush will wait and prevent the program from continuing until all the bytes 
+        are written to the connection; 
+        TcpStream contains an internal buffer to minimize calls to the underlying operating system.
+         */
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        }
+    else
+        {
+        // some other request.
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        stream.write(status_line.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        }
+    }
